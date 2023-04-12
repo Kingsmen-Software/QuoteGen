43d4b24bce14c0ebff6697be4a0e39ba5511c58c
Parker Horn <parker.horn@kingsmensoftware.com>
2023-04-12 09:53:22 -0400

    Initial commit

---
+++ gui/src/App.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+
+const App = () => {
+  const [closingActor, setClosingActor] = useState('');
+  const [closingActorList, setClosingActorList] = useState([]);
+  const [closingLine, setClosingLine] = useState('');
+  const [generatedSpeech, setGeneratedSpeech] = useState('');
+  const [openingActor, setOpeningActor] = useState('');
+  const [openingActorList, setOpeningActorList] = useState([]);
+  const [openingLine, setOpeningLine] = useState('');
+
+
+  useEffect(() => {
+    fetchActorList();
+  }, []);
+
+  const fetchActorList = async () => {
+    try {
+      const openingActors = await axios.get('http://localhost:3001/api/actors?type=opening');
+      const closingActors = await axios.get('http://localhost:3001/api/actors?type=closing');
+      setOpeningActorList(openingActors.data);
+      setClosingActorList(closingActors.data);
+    } catch (error) {
+      console.error('Error fetching actor list:', error);
+    }
+  };
+
+  const fetchSpeechLines = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/api/speech-lines', {
+        params: {
+          openingActor,
+          closingActor,
+        },
+      });
+      setOpeningLine(response.data.openingLine);
+      setClosingLine(response.data.closingLine);
+
+      // Generate the speech content
+      const speechResponse = await axios.post('http://localhost:3001/api/generate-speech', {
+        openingLine: response.data.openingLine,
+        closingLine: response.data.closingLine,
+      });
+
+      console.log('Generated speech content:', speechResponse.data);
+      setGeneratedSpeech(speechResponse.data);
+    } catch (error) {
+      console.error('Error fetching speech lines:', error);
+    }
+  };
+
+  return (
+    <div className="app-container">
+      <h1>Speech Generator</h1>
+      <div className="form-section">
+        <label htmlFor="openingActor">Opening Actor: </label>
+        <select
+          id="openingActor"
+          value={openingActor}
+          onChange={(e) => setOpeningActor(e.target.value)}
+        >
+          <option value="">Select an actor</option>
+          {openingActorList.map((actor) => (
+            <option key={actor} value={actor}>
+              {actor}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="form-section">
+        <label htmlFor="closingActor">Closing Actor: </label>
+        <select
+          id="closingActor"
+          value={closingActor}
+          onChange={(e) => setClosingActor(e.target.value)}
+        >
+          <option value="">Select an actor</option>
+          {closingActorList.map((actor) => (
+            <option key={actor} value={actor}>
+              {actor}
+            </option>
+          ))}
+        </select>
+      </div>
+      <p>Opening line: {openingLine}</p>
+      <p>Closing line: {closingLine}</p>
+      <button
+        onClick={fetchSpeechLines}
+        disabled={!openingActor || !closingActor}
+      >
+        Generate new speech lines
+      </button>
+      <div className="generated-speech">
+        <h2>Generated Speech</h2>
+        <textarea
+          value={generatedSpeech}
+          readOnly
+          rows={10}
+          cols={50}
+          style={{ resize: 'none' }}
+        ></textarea>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const openingLines = require('./openingLines.json');
+const closingLines = require('./closingLines.json');
+require('dotenv').config();
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/speech-lines', (req, res) => {
+  const { openingActor, closingActor } = req.query;
+
+  if (!openingActor || !closingActor) {
+    return res.status(400).json({ error: 'Both openingActor and closingActor must be provided' });
+  }
+
+  const getRandomLine = (lines) => lines[Math.floor(Math.random() * lines.length)];
+
+  res.json({
+    openingLine: getRandomLine(openingLines[openingActor]),
+    closingLine: getRandomLine(closingLines[closingActor]),
+  });
+});
+
+app.get('/api/actors', (req, res) => {
+  const { type } = req.query;
+
+  if (type === 'opening') {
+    res.json(Object.keys(openingLines));
+  } else if (type === 'closing') {
+    res.json(Object.keys(closingLines));
+  } else {
+    res.status(400).json({ error: 'Invalid type parameter' });
+  }
+});
+
+app.post('/api/generate-speech', async (req, res) => {
+  const { openingLine, closingLine } = req.body;
+  //const myPrompt = `Write a paragraph that starts with openingLine and finishes with closingLine. Generate content that would logically connect the two sentences. openingLine: ${openingLine} closingLine: ${closingLine}`;
+
+  try {
+    
+    const response = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: `Write a paragraph that starts with openingLine and finishes with closingLine. Generate content that would logically connect the two sentences. openingLine: ${openingLine} closingLine: ${closingLine}`,
+      max_tokens: 256,
+      n: 1,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    res.json(response.data.choices[0].text);
+    
+  } catch (error) {
+    console.error('Error generating speech:', error); // Add this line to log the error
+    res.status(500).json({ error: 'Error generating speech' });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
